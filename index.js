5005582cad1f5f1004d72f0c5e1f480613b3fbf3
sagorforu <freelancersagor18@gmail.com>
2023-05-02 14:39:30 +0600

    all data send

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const app = express();
-const port = 5000;
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+
+const chefData = require('./Data/chefData.json')
+
+app.use(cors())
 
 app.get("/", (req, res) => {
   res.send("food wise server running");
 });
+
+app.get('/chefdata', (req, res) => {
+    res.send(chefData)
+})
 
 app.listen(port, () => {
   console.log(`food wise server running on port:${port}`);
